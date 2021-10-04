d6aab933844fc40f40e61bd74a1a85a5fab73c38
hoanglong123000 <hoanglong123000@gmail.com>
2021-10-04 19:42:48 +0700

    -Adding function to Register button. -Now you can fill all register's information.

--- app/src/main/java/com/example/formdangnhapdangky/MainActivity.java
+++ app/src/main/java/com/example/formdangnhapdangky/MainActivity.java
@@ -3,28 +3,69 @@ package com.example.formdangnhapdangky;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView ToRegisterPage;
+
+    private  EditText USERNAME;
+    private  EditText PASSWORD;
+    private TextView BackToLoginPage;
+    private Button Regbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToRegisterPage = (TextView) findViewById(R.id.registerlink);
-        ToRegisterPage.setOnClickListener(new View.OnClickListener() {
+
+        USERNAME =  (EditText) findViewById(R.id.registerusername);
+        PASSWORD =  (EditText) findViewById(R.id.registerpasswordtxt);
+
+
+
+
+        // Return to Login page.
+        BackToLoginPage = (TextView) findViewById(R.id.backtoLoginLink);
+        BackToLoginPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                OpenRegisterPage();
+                BacktoMainActivity();
             }
         });
+
+        // Function of Register button.
+        Regbtn = (Button) findViewById(R.id.registerbtn);
+        Regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Regisbtn();
+            }
+        });
+
+
     }
 
-    public void OpenRegisterPage()
+    public void Regisbtn()
     {
-        Intent toRegisterpage = new Intent(this, RegisterActivity.class);
-        startActivity(toRegisterpage);
+        Intent finnishedreg = new Intent(this, RegisterActivity.class);
+        Bundle a = new Bundle();
+        a.putString("username", USERNAME.getText().toString());
+        a.putString("password", PASSWORD.getText().toString());
+        finnishedreg.putExtras(a);
+
+        startActivity(finnishedreg);
     }
+
+    // function of back to login page.
+    public void BacktoMainActivity()
+    {
+        Intent backtoMainActivity = new Intent(this, RegisterActivity.class);
+
+        startActivity(backtoMainActivity);
+    }
+
 }
--- app/src/main/java/com/example/formdangnhapdangky/RegisterActivity.java
+++ app/src/main/java/com/example/formdangnhapdangky/RegisterActivity.java
@@ -4,31 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
-    private TextView BackToLoginPage;
+
+    private TextView Reglink;
+    private EditText name;
+    private EditText pass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        BackToLoginPage = (TextView) findViewById(R.id.BacktoLoginLink);
-        BackToLoginPage.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_login);
+
+//Transfer data in Bundle to username textview and password textview in Register activity.
+        name =  (EditText) findViewById(R.id.nametxt);
+        pass =  (EditText) findViewById(R.id.passwordtxt);
+
+        Reglink = (TextView) findViewById(R.id.registerlink);
+        Bundle a = getIntent().getExtras();
+        name.setText(a.getString("username", "Default"));
+        pass.setText(a.getString("password", "Default"));
+
+
+        // Complete register page.
+        Reglink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    BacktoMainActivity();
+                Regbtn();
             }
         });
 
     }
 
-    public void BacktoMainActivity()
-    {
-        Intent backtoMainActivity = new Intent(this, MainActivity.class);
-        startActivity(backtoMainActivity);
-    }
 
+    // Function of register button.
+    public void Regbtn()
+    {
+        Intent completeregister = new Intent(this, MainActivity.class);
+        // Create a package bundle to store username textview and password textview from Register activity.
+
+        startActivity(completeregister);
+    }
 
 
 }
